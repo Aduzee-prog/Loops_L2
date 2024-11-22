dd1cf82ce11df1efed501aed130a1c9c51bf0bed
Aduzee-prog <zeenatayomiposi@gamil.com>
2024-11-22 22:42:47 +0100

    working good

---
+++ script.js
@@ -0,0 +1,28 @@
+// for(startingValu; CSSConditionRule; step) {
+//     Your code
+// }
+
+for(var i=1; i<=5; i++){
+    console.log(i);
+    hee.innerHTML += `<p style="color: red;" title ='ise nla'>${i} is a number</p>` 
+}
+
+var food = "potato"
+var drink = "palm wine"
+var student = "DCP"
+console.log(`The name of the student is DCP ${student} and his favorite food is ${food} and his best drink is ${drink}`);
+
+// for(var i=100; i>=1; i--){
+//     console.log(i);    
+// }
+// for(var i=-1; i>=-50; i--){
+//     console.log(i);  
+// }
+// for(i=1; i<=100; i++){
+//     if(i%2 == 0){
+//         console.log(i + " is an even number"); 
+//     }else{
+//         console.log(i + " is an odd numder");
+        
+//     }
+// }
